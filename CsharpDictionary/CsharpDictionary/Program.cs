7048caaa85fde925f426d3124a19b42a17a8eb28
Ashirbad chand <chand.ash3@gmail.com>
2016-04-26 16:16:51 +0100

    CsharpDictionary

--- CsharpDictionary/CsharpDictionary/Program.cs
+++ CsharpDictionary/CsharpDictionary/Program.cs
@@ -11,44 +11,74 @@ namespace CsharpDictionary
         static void Main(string[] args)
         {
 
-           
-            ////declare the collection of coding languages
-            Dictionary<string, string> codingLanguages = new Dictionary<string, string>();
-            string answer;
-            
-            //declaring the key & value
-            codingLanguages.Add("Java", "Back end programming language for advanced programming");
-            codingLanguages.Add("C#", "Back end programing language for better programming experience");
-            codingLanguages.Add("Javascript", "Popular Frontend programming language");
+                ////declare the collection of coding languages
+                Dictionary<string, string> codingLanguages = new Dictionary<string, string>();
+                string answer;
 
-            Console.WriteLine("coding language dictionary: \r\n type K to search,\r\n type D for the description,\r\n type C to count the list,\r\n type X to exit");
+                //declaring the key & value
+                codingLanguages.Add("Python", "Back end programming language is a open source programming language");
+                codingLanguages.Add("C#", "Back end programing language for better programming experience");
+                codingLanguages.Add("Javascript", "Popular Frontend programming language");
 
-            //get userinput
-            char response = Convert.ToChar(Console.ReadLine().ToUpper());
+                Console.WriteLine("coding language dictionary: \r\n type K to search,\r\n type D for the description,\r\n type C to count the list,\r\n type X to exit");
 
-            do
+                //get userinput
+                char response = Convert.ToChar(Console.ReadLine().ToUpper());
 
-            {
-              switch(response)
-                {
-                    case "K":
-                        Console.WriteLine("type the name if you would like to search for");
-                        answer = Console.ReadLine();
-                        break;
+                    do
+                    {
+                        switch (response)
+                        {
+                            case 'K':
+                                //input to search for the type of language
+                                Console.WriteLine("type of language if you would like to search for");
+                                answer = Console.ReadLine();
 
+                                if (codingLanguages.ContainsKey(answer))
+                                {
+                                    Console.WriteLine(answer + " is on the dictionary");
+                                }
+                                else
+                                {
+                                    Console.WriteLine(answer + " is not in the dictionary");
 
-                        
-                        
-                        break;
+                                }
+                                break;
 
+                            case 'D':
+                                //input for description
+                                Console.WriteLine("type for the description");
+                                answer = Console.ReadLine();
 
+                                if (codingLanguages.ContainsValue(answer))
+                                {
+                                    Console.WriteLine(answer + " is in the dictionary");
 
-                }
+                                }
+                                else
+                                {
+                                    Console.WriteLine(answer + " is not in the dictionary");
 
+                                }
+                                break;
 
+                            case 'C':
+                                //get total number of items in the dictionary
+                                int totalitems = codingLanguages.Count();
+                                Console.WriteLine("there are " + totalitems + " items in the dictionary");
+                                break;
 
+                            default:
+                                break;
+                        }
+                        // get userinput to test while loop
+                        Console.WriteLine("Please make another selection: ");
+                        response = Convert.ToChar(Console.ReadLine().ToUpper());
 
-            }
-        }
-    }
-}
+                 }
+                    while (response != 'X');
+                    
+                    }//end of dictionary program
+
+            }//end of program
+        }//end of namespace
