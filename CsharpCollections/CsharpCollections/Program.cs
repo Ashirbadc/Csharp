e6c5a52781b241afd1dd2363e5b2f59e04201e79
Ashirbad chand <chand.ash3@gmail.com>
2016-04-26 12:46:30 +0100

    CollectionsList

---
+++ CsharpCollections/CsharpCollections/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //to store the userinput
+            string bookToAdd, bookToDelete;
+
+            //collecting the list of books
+            List<string> mybook = new List<string>() { "To kill the mocking bird", "C# from scratch", "Javascript from scratch", "Java from scratch" };
+
+            //give user the instructions of choices
+            Console.WriteLine("Here is my collection of book. \r\n To add press A, \r\n To delete press D,\r\n To list the book press L, \r\n To sort press S, \r\n To exit press X");
+            //get user input
+            char response = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            // do while the response not equall to x
+            do
+            {
+
+                switch (response)
+                {
+                    case 'A':
+                        //prompt user to add book
+                        Console.WriteLine("Would you like to add a book?");
+                        bookToAdd = Console.ReadLine();
+                        //add the book to list in the end
+                        mybook.Add(bookToAdd);
+                        break;
+
+                    case 'D':
+                        //prompt user to delete book
+                        Console.WriteLine("Would you will to delete the book");
+                        bookToDelete = Console.ReadLine();
+                        // remove the book
+                        mybook.Remove(bookToDelete);
+                        break;
+
+                    case 'L':
+                        //Listing all the books
+                        for (int i = 0; i < mybook.Count; i++)
+                        {
+                            Console.WriteLine(mybook[i]);
+                        }
+                        break;
+                    case 'S':
+                        //sorting the books
+                        mybook.Sort();
+                        for (int i = 0; i < mybook.Count; i++)
+                        {
+                            Console.WriteLine(mybook[i]);
+                        }
+                     
+                        break;
+
+                    default:
+                        break;
+                }
+                //give user the instructions of choices
+                Console.WriteLine("What would you like to do next? \r\n To add press A, \r\n To delete press D,\r\n To list the book press L, \r\n To sort press S, \r\n To exit press X");
+                // get userinput to test the while loop conditions        
+                response = Convert.ToChar(Console.ReadLine().ToUpper());
+            }
+            //it will stop from running through the loops once user inputs X & exits
+            while (response != 'X');
+
+        
+
+
+
+
+        }//END OF COLLECTION
+    }//END OF PROGRAM
+}//END OF NAMESPACE
---
+++ CsharpDictionary/CsharpDictionary/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            ////declare the collection of coding languages
+            Dictionary<string, string> codingLanguages = new Dictionary<string, string>();
+            string answer;
+            
+            //declaring the key & value
+            codingLanguages.Add("Java", "Back end programming language for advanced programming");
+            codingLanguages.Add("C#", "Back end programing language for better programming experience");
+            codingLanguages.Add("Javascript", "Popular Frontend programming language");
+
+            Console.WriteLine("coding language dictionary: \r\n type K to search,\r\n type D for the description,\r\n type C to count the list,\r\n type X to exit");
+
+            //get userinput
+            char response = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            do
+
+            {
+              switch(response)
+                {
+                    case "K":
+                        Console.WriteLine("type the name if you would like to search for");
+                        answer = Console.ReadLine();
+                        break;
+
+
+                        
+                        
+                        break;
+
+
+
+                }
+
+
+
+
+            }
+        }
+    }
+}
